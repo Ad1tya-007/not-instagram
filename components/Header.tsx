729410849de6b6f702b722ce86c96b6498ae3087
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-22 16:14:40 -0700

    added header
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
 import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/solid'
 
 export default function Header() {
   return (
-    <div className="flex max-w-6xl justify-between">
-      <div>
+    <div>
+      <div className="flex max-w-6xl justify-between">
         {/* LOGO LEFT SIDE*/}
-        <div className="relative hidden h-24 w-24 lg:inline-grid">
+        <div className="relative hidden h-24 w-24 cursor-pointer lg:inline-grid">
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
             objectFit="contain"
           />
         </div>
-        <h1>SFU.NET</h1>
+
+        <div className="relative h-10 w-10 flex-shrink-0 cursor-pointer lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* MIDDLE : SEARCH BAR */}
+        <div className="relative mt-1 rounded-md p-3">
+          <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+            <SearchIcon className="h-5 w-5 text-gray-400" />
+          </div>
+          <input
+            className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
       </div>
     </div>
   )
