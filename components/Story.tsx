84a0309469d47e30045c5635c88bd9ca042afd23
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-24 11:59:31 -0700

    display stories
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Story.tsx
+++ components/Story.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 export default function Story({ img, username }) {
   return (
     <div>
-      <img src={img} alt="" />
-      <p>{username}</p>
+      <img
+        className="p-(1.5px) transform-200 h-14 w-14 cursor-pointer rounded-full border-2 border-red-500 object-contain transition ease-out hover:scale-110"
+        src={img}
+        alt=""
+      />
+      <p className="w-14 truncate text-center text-xs">{username}</p>
     </div>
   )
 }
