7e85bf7a6ac85342525fd5668f867fe66263e18c
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-24 23:54:52 -0700

    added Posts to Feed
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+import Posts from './Posts'
 import Stories from './Stories'
 
 export default function Feed() {
@@ -5,6 +6,7 @@ export default function Feed() {
     <main className="max-auto mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
       <section className="col-span-2">
         <Stories />
+        <Posts />
       </section>
       <section></section>
     </main>
