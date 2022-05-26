46c9a0bb5fb926fb16516fa62fd33061681b45fb
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-26 15:46:11 -0700

    created miniprofile
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

---
+++ components/MiniProfile.tsx
@@ -0,0 +1,15 @@
+export default function MiniProfile() {
+  return (
+    <div className="ml-10 mt-14 flex items-center border bg-white p-3 shadow-sm">
+      <img
+        src="https://avatars.githubusercontent.com/u/50322978?v=4"
+        alt="profile-pic"
+        className="h-16 w-16 rounded-full border p-1"
+      />
+      <div className="flex-1 p-3">
+        <h2 className="font-bold">Adi</h2>
+        <h3 className="text-sm">Welcome to Instagram!</h3>
+      </div>
+    </div>
+  )
+}
