326d398b2bd9422b4e77695f5c8d61dcf41d6a20
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-27 12:43:57 -0700

    small changes to suggestions
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Suggestions.tsx
+++ components/Suggestions.tsx
@@ -16,7 +16,7 @@ export default function Suggestions() {
   }, [])
 
   return (
-    <div className="ml-10 rounded-sm border border-gray-200 p-5 ">
+    <div className="ml-10 rounded-sm border border-gray-200 bg-white p-5">
       <div className="mb-3 flex justify-between">
         <p className="font-semibold text-gray-400">Suggestions</p>
         <button className="text-gray-400">See All</button>
@@ -27,12 +27,14 @@ export default function Suggestions() {
           className="mt-3 flex items-center justify-between"
         >
           <img
-            className="p-(2px) h-10 w-10 rounded-full border"
+            className="p-(1.5px) transform-200 h-10 w-10 cursor-pointer rounded-full border-2 border-red-500 object-contain transition ease-out hover:scale-110"
             src={profile.img}
             alt=""
           />
           <div className="ml-4 flex-1">
-            <h2 className="hover:underline">{profile.username}</h2>
+            <h2 className="cursor-pointer hover:underline">
+              {profile.username}
+            </h2>
             <h3 className="text-xs">Works at {profile.company}</h3>
           </div>
           <button className="text-xs text-blue-400">Follow</button>
