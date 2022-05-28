66f6ac8d8c0d5c13c9916cfac630f94b1a5a2f1c
Ad1tya-007 <adityakul0314@gmail.com>
2022-05-28 16:37:50 -0700

    added firebase.config
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCxWN6rR8PtofVNmxqDlb5ijIMKlN3dF5g',
+  authDomain: 'instagram-clone-ed700.firebaseapp.com',
+  projectId: 'instagram-clone-ed700',
+  storageBucket: 'instagram-clone-ed700.appspot.com',
+  messagingSenderId: '593768710246',
+  appId: '1:593768710246:web:04cd397d07353ee7206da7',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
