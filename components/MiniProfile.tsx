e9b65715ab70d7c5a17249e37a14b5ca7f3c1c4c
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-05 12:51:07 -0700

    added session and signOut to mini profile
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -1,17 +1,28 @@
+import { signOut, useSession } from 'next-auth/react'
+
 export default function MiniProfile() {
+  const { data: session, status } = useSession()
+
+  console.log(session)
+
   return (
     <div className="ml-10 mt-14 flex items-center border bg-white p-3 shadow-sm">
       <img
-        src="https://avatars.githubusercontent.com/u/50322978?v=4"
+        src={session?.user?.image}
         alt="profile-pic"
         className="h-16 w-16 rounded-full bg-blue-500 p-0.5"
       />
 
       <div className="flex-1 p-3">
-        <h2 className="font-bold">Adi</h2>
+        <h2 className="font-bold">{session?.user?.name}</h2>
         <h3 className="text-sm">Welcome to Not-Instagram!</h3>
       </div>
-      <button className="font-semibold text-blue-400">Sign Out</button>
+      <button
+        onClick={() => signOut({ callbackUrl: '/auth/signin' })}
+        className="font-semibold text-blue-400"
+      >
+        Sign Out
+      </button>
     </div>
   )
 }
