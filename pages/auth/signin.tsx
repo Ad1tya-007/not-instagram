084940fb78c506a7d1b6271a2b9047e713059906
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-05 12:51:31 -0700

    added little design to custom signin page
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -6,14 +6,24 @@ export default function signIn({ providers }) {
     <>
       <title>SignIn</title>
       <Header />
-
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <div className="-mt-40 flex min-h-screen flex-col items-center justify-center py-2">
+        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">Welcome to Not-Instagram!!!</p>
+        <div className="mt-40 flex">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name} className="p-3">
+              <button
+                className="w-30 items-center rounded-lg bg-blue-500 p-3 text-white"
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: '/' })
+                }
+              >
+                SignIn with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   )
 }
