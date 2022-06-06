f07aca6e029e170288f391ef07a989760e720446
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-06 10:14:11 -0700

    added custom session callback
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -19,7 +19,7 @@ export default NextAuth({
   callbacks: {
     async session({ session, token, user }) {
       session.user.username = session.user.name
-        .split('')
+        .split(' ')
         .join('')
         .toLocaleLowerCase()
 
