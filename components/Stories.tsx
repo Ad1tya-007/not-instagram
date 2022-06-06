9e3bfe0a9209c7f1fa4eeb399017fce10e89e3c9
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-06 14:32:37 -0700

    added plus sign under users story
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,9 +1,12 @@
 import faker from 'faker'
+import { useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
 import Story from './Story'
+import { PlusCircleIcon } from '@heroicons/react/solid'
 
 export default function Stories() {
   const [suggestions, setSuggestions] = useState([])
+  const { data: session, status } = useSession()
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -17,6 +20,11 @@ export default function Stories() {
 
   return (
     <div className="mt-2 flex space-x-6 overflow-x-scroll rounded-sm border-gray-200 p-6 scrollbar-hide">
+      <div>
+        <Story img={session?.user?.image} username={session?.user?.username} />
+        <PlusCircleIcon className="absolute -mt-9 ml-10 h-5 w-5 text-blue-500" />
+      </div>
+
       {suggestions.map((profile) => (
         <Story key={profile.id} img={profile.img} username={profile.username} />
       ))}
