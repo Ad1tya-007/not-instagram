ff7e60e4b7df358c04a70a8761831e10a1812dc9
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-06 10:13:37 -0700

    added username to miniprofile
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/MiniProfile.tsx
+++ components/MiniProfile.tsx
@@ -3,8 +3,6 @@ import { signOut, useSession } from 'next-auth/react'
 export default function MiniProfile() {
   const { data: session, status } = useSession()
 
-  console.log(session)
-
   return (
     <div className="ml-10 mt-14 flex items-center border bg-white p-3 shadow-sm">
       <img
@@ -14,7 +12,7 @@ export default function MiniProfile() {
       />
 
       <div className="flex-1 p-3">
-        <h2 className="font-bold">{session?.user?.name}</h2>
+        <h2 className="font-bold">{session?.user?.username}</h2>
         <h3 className="text-sm">Welcome to Not-Instagram!</h3>
       </div>
       <button
