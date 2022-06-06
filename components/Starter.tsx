442b5505dcc6234d2b73f0fe103d586894477048
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-06 16:51:21 -0700

    fixed some errors
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Starter.tsx
+++ components/Starter.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function Starter() {
-  return (
-    <div className="">
-      <div className="flex min-h-screen flex-col items-center justify-center  py-2">
-        <img
-          className="-mt-20 w-80"
-          src="https://links.papareact.com/ocw"
-          alt=""
-        />
-        <p className="font-xs text-red animate-pulse italic">
-          Welcome to Not-Instagram!!!
-        </p>
+  const router = useRouter()
 
-        <button
-          className="mt-5 rounded-lg bg-blue-500 p-3 text-white"
-          onClick={() => window.location.replace('/auth/signin')}
-        >
-          SignIn
-        </button>
-      </div>
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <img
+        className="-mt-20 w-80"
+        src="https://links.papareact.com/ocw"
+        alt=""
+      />
+      <p className="font-xs text-red animate-pulse italic">
+        Welcome to Not-Instagram!!!
+      </p>
+
+      <button
+        className="mt-5 rounded-lg bg-blue-500 p-3 text-white"
+        onClick={() => router.push('/auth/signin')}
+      >
+        SignIn
+      </button>
     </div>
   )
 }
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -5,23 +5,31 @@ export default function signIn({ providers }) {
   return (
     <>
       <title>SignIn</title>
-      <Header />
-      <div className="-mt-40 flex min-h-screen flex-col items-center justify-center py-2">
-        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
-        <p className="font-xs italic">Welcome to Not-Instagram!!!</p>
-        <div className="mt-40 flex">
-          {Object.values(providers).map((provider) => (
-            <div key={provider.name} className="p-3">
-              <button
-                className="w-30 items-center rounded-lg bg-blue-500 p-3 text-white"
-                onClick={() =>
-                  SignIntoProvider(provider.id, { callbackUrl: '/' })
-                }
-              >
-                SignIn with {provider.name}
-              </button>
-            </div>
-          ))}
+      <div className="bg-gradient-to-tr from-yellow-500 to-fuchsia-500 bg-cover">
+        <div className="flex min-h-screen items-center justify-center py-2">
+          <div className="">
+            <img
+              className=" w-80"
+              src="https://links.papareact.com/ocw"
+              alt=""
+            />
+            <p className="text-center italic">Welcome to Not-Instagram!!!</p>
+          </div>
+
+          <div className="">
+            {Object.values(providers).map((provider) => (
+              <div key={provider.name} className="p-3">
+                <button
+                  className="w-30 items-center rounded-lg bg-blue-500 p-3 text-white"
+                  onClick={() =>
+                    SignIntoProvider(provider.id, { callbackUrl: '/' })
+                  }
+                >
+                  SignIn with {provider.name}
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
