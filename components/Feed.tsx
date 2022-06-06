1467f0df8d26626f04fe7f1be0ac7cc5197d4cd7
Ad1tya-007 <adityakul0314@gmail.com>
2022-06-06 10:13:06 -0700

    added custom page if user is not authenticated
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,22 +1,37 @@
 import { useSession } from 'next-auth/react'
 import MiniProfile from './MiniProfile'
 import Posts from './Posts'
+import Starter from './Starter'
 import Stories from './Stories'
 import Suggestions from './Suggestions'
 
 export default function Feed() {
+  const { data: session, status } = useSession()
   return (
-    <main className="max-auto mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
-      <section className="col-span-2">
-        <Stories />
-        <Posts />
-      </section>
-      <section className="hidden md:col-span-1 xl:inline-grid">
-        <div className="fixed top-20 ">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+    <main
+      className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3 ${
+        !session && '!max-w-3xl !grid-cols-1'
+      }`}
+    >
+      {session && (
+        <section className="col-span-2">
+          <Stories />
+          <Posts />
+        </section>
+      )}
+      {session && (
+        <section className="hidden md:col-span-1 xl:inline-grid">
+          <div className="fixed top-20 ">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
+      {!session && (
+        <section className="col-span-2 ">
+          <Starter />
+        </section>
+      )}
     </main>
   )
 }
